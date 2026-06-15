60f0ba1e38efd09b58dece4f100bd445c283915b
mohdaquif07 <ahmadgayyur2@gmail.com>
2026-06-15 23:51:09 +0530

    learn about context api

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,13 @@
+import UserContextProvider from "./context/UserContextProvider"
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+    <h1>React with chaai</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
