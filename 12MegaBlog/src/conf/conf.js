e0dcb5765c0141444e4dd60a9c7f8d68740faf73
mohdaquif07 <ahmadgayyur2@gmail.com>
2026-06-19 22:34:32 +0530

    Blog project started

---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+
+APPWRITEURL: String(import.meta.env.VITE_APPWRITE_URL),
+APPWRITEPROJECTID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+APPWRITEDATABASEID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+APPWRITETABLEID: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+APPWRITEBUCKETID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+
+export default conf
